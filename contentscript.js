9187fea228397e821d5f4132b40b0a862ba9e244
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2015-10-04 17:40:37 +0300

    get text from URL

--- contentscript.js
+++ contentscript.js
@@ -1,9 +1,20 @@
 setTimeout(function() {
-	var input = document.querySelector('.gsfi'),
-		text = input && input.value,
+
+	var text = getUrlVars()['q'],
 		links = document.querySelectorAll('.bia.uh_rl');
 
-	for (var i = 0; i < links.length && input; i++) {
+	for (var i = 0; i < links.length; i++) {
 		links[i].href = 'https://yandex.ru/images/search?text=' + text + '&source=z-google-images';
+	}
+	
+	// console.log('updated links', links.length);
+
+	function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+			function(m, key, value) {
+				vars[key] = value;
+			});
+		return vars;
 	}
 }, 1000);
